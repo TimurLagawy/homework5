29add5448e242644cb7a3e5135bf87b3e2412cfd
TumurLagawy <timurtagirovich@mail.ru>
2022-03-21 18:12:16 +0300

    Homework5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String args[]) {
-        Student student1 = new Student(Петя, Иванов, 21, 4, true);
-        Student student2 = new Student(Мария, Лепешко, 18, 1, false);
-        Student student3 = new Student(Виктор, Макаревич, 19, 2, true);
-        Student student4 = new Student(Оксана, Петрова, 17, 1, true);
-        Head_teacher head_teacher = new Head_teacher(Мария, Жукова, 42);
+        Student student1 = new Student("Петя", "Иванов", 20, 4, true);
+        Student student2 = new Student("Мария", "Лепешко", 18, 1, false);
+        Student student3 = new Student("Виктор", "Макаревич", 19, 2, true);
+        Student student4 = new Student("Оксана", "Петрова", 17, 1, true);
+        Head_teacher head_teacher = new Head_teacher("Мария", "Жукова", 40);
 
         student1.say();
         student2.say();
