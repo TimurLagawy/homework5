4c57fb0888a80374ee97970e750b762da5901cd8
TumurLagawy <timurtagirovich@mail.ru>
2022-03-27 21:42:42 +0300

    Homework5 added task_for_time

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,9 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestClass {
-    public static void sleep (long millSec) {
-        try {
-            Thread.sleep(millSec);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    @Test
+    public void someTest() {
+        int actualValue = getSum(6, 6);
+
+        Assert.assertEquals(actualValue, 12, "method sum not work");
+    }
+    public static int getSum(int a, int b){
+        return  a+b;
+
     }
 }
